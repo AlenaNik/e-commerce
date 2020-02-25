7a3c1e68a7f966270cf6eae380c41cdfbb39d329
Alena Nikolaeva <alenanikolaeva@PX369-ANIKOLAEVA.local>
2020-02-25 16:10:32 +0100

    comp

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,15 @@
-import App, { Container } from 'next/app';
+import React from 'react'
+import App from 'next/app';
 import Page from '../components/Page'
 
 class MyApp extends App {
     render() {
         const { Component } = this.props
         return (
-            <Container>
-                <p>Hey I'm on every page</p>
-                <Component/>
-            </Container>
+                <div>
+                    <p>Hey I'm on every page</p>
+                    <Component />
+                </div>
         )
     }
 }
