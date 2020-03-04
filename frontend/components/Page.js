207a3d06f8e13ba61d120707d55aadfd4190d890
AlenaNik <alena.nik.digital@gmail.com>
2020-03-04 16:47:15 +0100

    styled button test

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 import { Header } from './Header'
+import { Meta } from './Meta'
+import styled from 'styled-components'
+
+const MyButton = styled.button`
+  -webkit-appearance: button;
+  -webkit-font-smoothing: antialiased;
+  background-color: #000;
+  background-image: none;
+  border: 0 solid transparent;
+  border-radius: 2px;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-size: 12px;
+  font-weight: 400;
+  height: 44px;
+  letter-spacing: .1em;
+  line-height: 0;
+  margin: 0 0 25px;
+  outline: none;
+  padding: 6px 20px;
+  position: relative;
+  text-align: center;
+  text-transform: uppercase;
+  vertical-align: top;
+  width: auto;
+
+`;
+
+
 class Page extends Component {
     render() {
         return (
             <div>
+                <Meta/>
                 <Header/>
+                <MyButton>Click me</MyButton>
                 <p>This is the page component</p>
                     {this.props.children}
             </div>
